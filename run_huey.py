f558c148626dcefe314a744184b379d82cb3153d
Carlos Parra Camacho <carlosparracamacho@gmail.com>
2026-03-25 16:44:47 +0100

    Fix: Bug on similar items | Dev: Email when purchasing an item

--- run_huey.py
+++ run_huey.py
@@ -2,6 +2,5 @@ from utils import huey
 from app import app
 
 if __name__ == '__main__':
-    # Esto permite ejecutar el consumer directamente con: python run_huey.py
-    # Aunque lo normal es usar: huey_consumer.py run_huey.huey
+    #python -m huey.bin.huey_consumer run_huey.huey
     pass
